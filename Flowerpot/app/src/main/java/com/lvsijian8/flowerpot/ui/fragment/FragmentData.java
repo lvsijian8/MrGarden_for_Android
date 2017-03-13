49f8984dcbb66a8306aa10e280c38580eacf8362
lvsijian8 <499365864@qq.com>
2017-03-13 16:54:01 +0800

    界面美化

--- Flowerpot/app/src/main/java/com/lvsijian8/flowerpot/ui/fragment/FragmentData.java
+++ Flowerpot/app/src/main/java/com/lvsijian8/flowerpot/ui/fragment/FragmentData.java
@@ -1,5 +1,7 @@
 package com.lvsijian8.flowerpot.ui.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
@@ -15,8 +17,11 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.lvsijian8.flowerpot.R;
+import com.lvsijian8.flowerpot.ui.view.CircleImageView;
 import com.lvsijian8.flowerpot.ui.view.ProgressArc;
 import com.lvsijian8.flowerpot.ui.view.TasksCompletedView;
 import com.lvsijian8.flowerpot.utils.UIUtils;
@@ -36,11 +41,14 @@ import lecho.lib.hellocharts.view.LineChartView;
 
 public class FragmentData extends Fragment {
 	private LinearLayout layout_contain;
-	private LineChartView lineTemperature;
+	private LineChartView lineTemperature;//温度
+	private LineChartView lineHumidityView;//湿度
 	private PopupWindow mPopupWindow;
 	private ImageView btn_edit;
 //	private LineChartData lineChartData;
 	private float[] f={22.2f,30.0f,30.5f,19.4f,25.0f,24.0f,22.5f,28.6f,19.2f,25.0f,26.4f,23.7f};
+	private CircleImageView circle_icon;
+	private RadioGroup group;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -48,22 +56,21 @@ public class FragmentData extends Fragment {
 		View view=inflater.inflate(R.layout.fragment_data, container, false);
 		initui(view);
 		setPopup();
-		Log.e("ZDLW","W");
-
 		return view;
 	}
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		setTemperature(lineTemperature, f);
+		setLineView(lineTemperature, f);
+		setLineView(lineHumidityView, f);
 	}
 
 	private void initui(View view){
 		layout_contain= (LinearLayout) view.findViewById(R.id.layout_fdata_contain);
+		circle_icon = (CircleImageView) view.findViewById(R.id.circle_fdata_icon);
 		lineTemperature= (LineChartView) view.findViewById(R.id.chart_data_temperature);
-		TasksCompletedView conview= (TasksCompletedView) view.findViewById(R.id.tasks_view);
-		conview.setProgress(30);
+		lineHumidityView= (LineChartView) view.findViewById(R.id.chart_data_humidity);
 		btn_edit= (ImageView) view.findViewById(R.id.ibtn_data_edit);
 		btn_edit.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -77,12 +84,18 @@ public class FragmentData extends Fragment {
 				}
 			}
 		});
+		circle_icon.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				setAlert(2);
+			}
+		});
 	}
 
 	/**
 	 * 设置温度LineChart
 	 */
-	private void setTemperature(LineChartView lineTemperature,float[] data){
+	private void setLineView(LineChartView lineTemperature,float[] data){
 		lineTemperature.setInteractive(true);//是否与用户交互
 		lineTemperature.setZoomEnabled(true);//是否可以缩放
 		lineTemperature.setValueSelectionEnabled(true);//是否有点击动画
@@ -112,9 +125,9 @@ public class FragmentData extends Fragment {
 			line.setColor(R.color.violet);
 			line.setFilled(true);//设置折线覆盖区域
 			line.setCubic(true);//设置曲线是否圆滑
-			line.setPointRadius(2);//设置圆点的半径
+			line.setPointRadius(3);//设置圆点的半径
 			line.setShape(ValueShape.CIRCLE);//节点图形样式 DIAMOND菱形、SQUARE方形、CIRCLE圆形
-			line.setPointColor(Color.RED);//设置节点颜色
+			line.setPointColor(getResources().getColor(R.color.skyblue));//设置节点颜色
 			line.setStrokeWidth(1);//线的宽度
 			line.setHasPoints(true);//是否显示节点
 			line.setHasLabels(false);//是否显示节点数据
@@ -132,7 +145,7 @@ public class FragmentData extends Fragment {
 			lineChartData.setValueLabelBackgroundEnabled(false);//点击后是字体是否背景色
 			lineChartData.setValueLabelsTextColor(Color.BLACK);//点击后的字体颜色
 			lineTemperature.setLineChartData(lineChartData);
-			resetViewport();
+			resetViewport(lineTemperature);
 		}
 
 
@@ -151,20 +164,83 @@ public class FragmentData extends Fragment {
 		mPopupWindow.setAnimationStyle(R.style.anim_menu_bottombar);//加载弹进弹出风格
 		mPopupWindow.getContentView().setFocusableInTouchMode(true);
 		mPopupWindow.getContentView().setFocusable(true);
+		setOnclickPopup(popupView);
+	}
+
+	/**
+	 * 设置Popup的点击事件
+	 */
+	private void setOnclickPopup(View popupView) {
+
 	}
 
 	/**
 	 * 注意，以下必须设置，v是整个坐标最大有多大，v.top=50,最高就是50.
 	 */
-	private void resetViewport() {
+	private void resetViewport(LineChartView lineTemperature) {
 		final Viewport v = new Viewport(lineTemperature.getMaximumViewport());
 		v.bottom = 0;
-		v.top = 52;
+		v.top = 42;
 		v.left = 0;
 		v.right = f.length - 1;
 		lineTemperature.setCurrentViewportWithAnimation(v);
 		lineTemperature.setMaximumViewport(v);
 		lineTemperature.setCurrentViewport(v);
+	}
+
+	/**
+	 * 用于选择需要检测数据的花盆
+	 * @param i
+	 */
+	private void setAlert(int i){
+		AlertDialog alertDialog=null;
+		final AlertDialog.Builder builder=new AlertDialog.Builder(getContext());
+		builder.setTitle("请选择您的花盆");
+		View choice_view=View.inflate(UIUtils.getContext(),R.layout.choice_flower,null);
+		group = (RadioGroup) choice_view.findViewById(R.id.rg_choice);
+		RadioButton r1= (RadioButton) choice_view.findViewById(R.id.rbtn_choice_one);
+		RadioButton r2= (RadioButton) choice_view.findViewById(R.id.rbtn_choice_two);
+		RadioButton r3= (RadioButton) choice_view.findViewById(R.id.rbtn_choice_three);
+		switch (i){
+			case 2:
+				r3.setVisibility(View.GONE);
+				break;
+			case 1:
+				r3.setVisibility(View.GONE);
+				r2.setVisibility(View.GONE);
+				break;
+			case 0:
+				r3.setVisibility(View.GONE);
+				r2.setVisibility(View.GONE);
+				r1.setVisibility(View.GONE);
+				break;
+			default:
+				break;
+		}
+		builder.setView(choice_view);
+
+		builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+
+			}
+		});
+		builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				int switchID = group.getCheckedRadioButtonId();
+				switch (switchID) {
+					case R.id.rbtn_choice_one:
+						break;
+					case R.id.rbtn_choice_two:
+						break;
+					case R.id.rbtn_choice_three:
+						break;
+				}
+			}
+		});
+		alertDialog=builder.create();
+		alertDialog.show();
 	}
 
 }
--- Flowerpot/app/src/main/java/com/lvsijian8/flowerpot/ui/fragment/FragmentFind.java
+++ Flowerpot/app/src/main/java/com/lvsijian8/flowerpot/ui/fragment/FragmentFind.java
@@ -5,13 +5,68 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 
 import com.lvsijian8.flowerpot.R;
+import com.lvsijian8.flowerpot.ui.view.Relistview;
+
+import java.util.ArrayList;
 
 public class FragmentFind extends Fragment {
-@Override
-public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	private ImageView ibtn_search;
+	private Relistview relistview;
+	private ArrayList<String> listdata;
+	private MyAdapter myAdapter;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 		Bundle savedInstanceState) {
-	return inflater.inflate(R.layout.fragment_find, container, false);
-}
+		View find_view=inflater.inflate(R.layout.fragment_find, container, false);
+		initUui(find_view);
+		initData();
+		return find_view;
+
+	}
+
+	private void initUui(View view) {
+		ibtn_search= (ImageView) view.findViewById(R.id.ibtn_find_search);
+		relistview= (Relistview) view.findViewById(R.id.lv_find_flower);
+		listdata=new ArrayList();
+		for (int i=0;i<23;i++){
+			listdata.add("蝴蝶"+i);
+		}
+		myAdapter = new MyAdapter();
+		relistview.setAdapter(myAdapter);
+	}
+
+	private void initData(){
+
+	}
+
+	class MyAdapter extends BaseAdapter{
+
+		@Override
+		public int getCount() {
+			return listdata.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return listdata.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+				View view=View.inflate(getContext(),R.layout.find_item,null);
+
+			return view;
+		}
+	}
 }
